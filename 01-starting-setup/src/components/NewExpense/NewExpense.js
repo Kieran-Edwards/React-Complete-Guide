4e2d8f05da6c4a1a3bb66bdf6bb2c139e73c2efd
Kieran <kedwards@salmon.com>
2022-11-30 21:23:26 +0000

    Assignment 4: Time to Practice: Conditional Content

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+    const [newExpenseOpen, setNewExpenseOpen] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString(),
         };
 
+        setNewExpenseOpen(!newExpenseOpen);
+
         props.onAddExpense(expenseData);
+    };
+
+    const newExpenseOpenHandler = () => {
+        setNewExpenseOpen(!newExpenseOpen);
     };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!newExpenseOpen && (
+                <button onClick={newExpenseOpenHandler}>Add New Expense</button>
+            )}
+            {newExpenseOpen && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onNewExpenseCancel={newExpenseOpenHandler}
+                />
+            )}
         </div>
     );
 };

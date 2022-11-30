97cf8c543ae12f15def31458bca5bb257b35ea37
Kieran <kedwards@salmon.com>
2022-11-30 10:25:48 +0000

    63. Rendering Lists of Data

--- 01-starting-setup/src/components/Expenses/ExpenseList.js
+++ 01-starting-setup/src/components/Expenses/ExpenseList.js
@@ -20,26 +20,14 @@ const ExpenseList = (props) => {
                     onFilterChange={filterChangeHandler}
                     selectedYear={selectedYear}
                 />
-                <ExpenseItem
-                    title={props.expenses[0].title}
-                    price={props.expenses[0].price}
-                    date={props.expenses[0].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[1].title}
-                    price={props.expenses[1].price}
-                    date={props.expenses[1].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[2].title}
-                    price={props.expenses[2].price}
-                    date={props.expenses[2].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[3].title}
-                    price={props.expenses[3].price}
-                    date={props.expenses[3].date}
-                />
+
+                {props.expenses.map((expense) => (
+                    <ExpenseItem
+                        title={expense.title}
+                        price={expense.price}
+                        date={expense.date}
+                    />
+                ))}
             </Card>
         </div>
     );

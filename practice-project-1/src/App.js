4d7645909aebb20e87ea37e9a7fa2f223916f75a
Kieran <kedwards@salmon.com>
2022-12-02 14:00:10 +0000

    Init my solution - no styling

--- practice-project-1/src/App.js
+++ practice-project-1/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
+import NewUserForm from "./Components/NewUserForm";
+import UserList from "./Components/UserList";
+import Modal from "./Components/Modal";
 
-function App() {
-  return (
-    <div>
+const App = () => {
+    const [users, setUsers] = useState([]);
+    const [modalState, setModalState] = useState(false);
+    const [modalContent, setModalContent] = useState({
+        title: "",
+        content: "",
+    });
 
-    </div>
-  );
-}
+    const addUserHandler = (user) => {
+        setUsers((prevUsers) => {
+            return [user, ...prevUsers];
+        });
+    };
+
+    const modalHandler = () => {
+        setModalState(!modalState);
+    };
+
+    return (
+        <div>
+            <NewUserForm
+                addUserHandler={addUserHandler}
+                modalHandler={modalHandler}
+                setModalContent={setModalContent}
+            />
+            {modalState && (
+                <Modal
+                    modalHandler={modalHandler}
+                    modalContent={modalContent}
+                />
+            )}
+            {users.length > 0 && <UserList users={users} />}
+        </div>
+    );
+};
 
 export default App;

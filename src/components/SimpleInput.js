a9f88267a109b1aa29b2620c2937d9b81eae4d01
Kieran <kedwards@salmon.com>
2023-01-06 10:51:36 +0000

    using hooks for validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,68 +1,50 @@
 import { useState } from "react";
 
+import useInput from "../hooks/use-input";
+
 const SimpleInput = (props) => {
-    const [enteredName, setEnteredName] = useState("");
-    const [enteredEmail, setEnteredEmail] = useState("");
-    const [inputNameIsTouched, setInputNameIsTouched] = useState(false);
-    const [inputEmailIsTouched, setInputEmailIsTouched] = useState(false);
+    const {
+        value: nameValue,
+        isValid: nameIsValid,
+        hasError: nameHasError,
+        changeHandler: nameChangeHandler,
+        touchHandler: nameTouchHandler,
+        reset: nameReset,
+    } = useInput((value) => value.trim() !== "");
 
-    const inputNameIsValid = enteredName.trim() !== "";
-    const inputEmailIsValid = /(.+)@(.+){2,}\.(.+){2,}/.test(enteredEmail);
-
-    const touchedNameInputIsInvalid = inputNameIsTouched && !inputNameIsValid;
-    const touchedEmailInputIsInvalid =
-        inputEmailIsTouched && !inputEmailIsValid;
+    const {
+        value: emailValue,
+        isValid: emailIsValid,
+        hasError: emailHasError,
+        changeHandler: emailChangeHandler,
+        touchHandler: emailTouchHandler,
+        reset: emailReset,
+    } = useInput((value) => /(.+)@(.+){2,}\.(.+){2,}/.test(value));
 
     let formIsValid = false;
 
-    if (inputNameIsValid && inputEmailIsValid) {
+    if (nameIsValid && emailIsValid) {
         formIsValid = true;
     }
-
-    const nameInputChangeHandler = (event) => {
-        setEnteredName(event.target.value);
-    };
-
-    const emailInputChangeHandler = (event) => {
-        setEnteredEmail(event.target.value);
-    };
-
-    const nameInputTouchHandler = () => {
-        setInputNameIsTouched(true);
-    };
-
-    const emailInputTouchHandler = () => {
-        setInputEmailIsTouched(true);
-    };
-
-    const resetForm = () => {
-        setEnteredName("");
-        setEnteredEmail("");
-
-        setInputNameIsTouched(false);
-        setInputEmailIsTouched(false);
-    };
 
     const formSubmissionHandler = (event) => {
         event.preventDefault();
 
-        nameInputTouchHandler();
-        emailInputTouchHandler();
-
-        if (!inputNameIsValid || !inputEmailIsValid) {
+        if (!nameIsValid || !emailIsValid) {
             return;
         }
 
-        console.log(enteredName);
+        console.log(nameValue, emailValue);
 
-        resetForm();
+        nameReset();
+        emailReset();
     };
 
-    const nameControlClass = touchedNameInputIsInvalid
+    const nameControlClass = nameHasError
         ? "form-control invalid"
         : "form-control";
 
-    const emailControlClass = touchedEmailInputIsInvalid
+    const emailControlClass = emailHasError
         ? "form-control invalid"
         : "form-control";
 
@@ -71,27 +53,25 @@ const SimpleInput = (props) => {
             <div className={nameControlClass}>
                 <label htmlFor="name">Your Name</label>
                 <input
-                    onChange={nameInputChangeHandler}
+                    onChange={nameChangeHandler}
                     type="text"
                     id="name"
-                    value={enteredName}
-                    onBlur={nameInputTouchHandler}
+                    value={nameValue}
+                    onBlur={nameTouchHandler}
                 />
-                {touchedNameInputIsInvalid && <p>Please enter a valid name</p>}
+                {nameHasError && <p>Please enter a valid name</p>}
             </div>
 
             <div className={emailControlClass}>
                 <label htmlFor="email">Your E-mail</label>
                 <input
-                    onChange={emailInputChangeHandler}
+                    onChange={emailChangeHandler}
                     type="text"
                     id="email"
-                    value={enteredEmail}
-                    onBlur={emailInputTouchHandler}
+                    value={emailValue}
+                    onBlur={emailTouchHandler}
                 />
-                {touchedEmailInputIsInvalid && (
-                    <p>Please enter a valid e-mail</p>
-                )}
+                {emailHasError && <p>Please enter a valid e-mail</p>}
             </div>
 
             <div className="form-actions">

03acf9681b3218409f25d30e46cff6b453f6a2f4
Kieran <kedwards@salmon.com>
2023-01-19 19:01:29 +0000

    using multiple slices + my solution for reading slices

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,23 +1,39 @@
-import classes from './Auth.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import { authActions } from "../store/index";
+
+import classes from "./Auth.module.css";
 
 const Auth = () => {
-  return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
-  );
+    const dispatch = useDispatch();
+    const auth = useSelector((state) => state.auth.isAuth);
+
+    const loginHandler = (e) => {
+        e.preventDefault();
+        dispatch(authActions.login());
+    };
+
+    if (!auth) {
+        return (
+            <main className={classes.auth}>
+                <section>
+                    <form onSubmit={loginHandler}>
+                        <div className={classes.control}>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" />
+                        </div>
+                        <div className={classes.control}>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" />
+                        </div>
+                        <button>Login</button>
+                    </form>
+                </section>
+            </main>
+        );
+    }
+
+    return "";
 };
 
 export default Auth;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
-import classes from './Header.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import { authActions } from "../store/index";
+
+import classes from "./Header.module.css";
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    const dispatch = useDispatch();
+    const auth = useSelector((state) => state.auth.isAuth);
+
+    const logoutHandler = () => {
+        dispatch(authActions.logout());
+    };
+
+    return (
+        <header className={classes.header}>
+            <h1>Redux Auth</h1>
+            {auth && (
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">My Products</a>
+                        </li>
+                        <li>
+                            <a href="/">My Sales</a>
+                        </li>
+                        <li>
+                            <button onClick={logoutHandler}>Logout</button>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </header>
+    );
 };
 
 export default Header;

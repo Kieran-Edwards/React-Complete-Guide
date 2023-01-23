4f014cb598e42ca0a08b7f3c02adecec32c637d5
Kieran <kedwards@salmon.com>
2023-01-23 13:07:10 +0000

    Fix issue with auth on app.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { authActions } from "../store/index";
 
@@ -6,34 +6,29 @@ import classes from "./Auth.module.css";
 
 const Auth = () => {
     const dispatch = useDispatch();
-    const auth = useSelector((state) => state.auth.isAuth);
 
     const loginHandler = (e) => {
         e.preventDefault();
         dispatch(authActions.login());
     };
 
-    if (!auth) {
-        return (
-            <main className={classes.auth}>
-                <section>
-                    <form onSubmit={loginHandler}>
-                        <div className={classes.control}>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" />
-                        </div>
-                        <div className={classes.control}>
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" />
-                        </div>
-                        <button>Login</button>
-                    </form>
-                </section>
-            </main>
-        );
-    }
-
-    return "";
+    return (
+        <main className={classes.auth}>
+            <section>
+                <form onSubmit={loginHandler}>
+                    <div className={classes.control}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" />
+                    </div>
+                    <button>Login</button>
+                </form>
+            </section>
+        </main>
+    );
 };
 
 export default Auth;

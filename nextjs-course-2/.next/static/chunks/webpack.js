7591ca86eb269ee0586db54e4498501c2e8867a0
Kieran <kedwards@salmon.com>
2023-02-13 10:53:56 +0000

    using the link component

--- nextjs-course-2/.next/static/chunks/webpack.js
+++ nextjs-course-2/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "738f68a7e3a1cf5f"; }
+/******/ 		__webpack_require__.h = function() { return "dc6a3014e3090349"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- nextjs-course-2/pages/news/index.js
+++ nextjs-course-2/pages/news/index.js
@@ -1,5 +1,18 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function NewsPage() {
-    return <h1>NewsPage</h1>;
+    return (
+        <Fragment>
+            <h1>NewsPage</h1>
+            <ul>
+                <li>
+                    <Link href="/news/link1">Link 1</Link>
+                </li>
+                <li>Link 2</li>
+            </ul>
+        </Fragment>
+    );
 }
 
 export default NewsPage;
